feed39182b5c0d96d21a3c5cb06788c4c05250a7
prashantkr658 <69711096+prashantkr658@users.noreply.github.com>
2020-08-15 22:08:30 +0530

    Update Array9.java

--- Array9.java
+++ Array9.java
@@ -9,7 +9,7 @@ public class Array9 {
 	public static void sumOfTwoElements(int [] arr) {
 		int minSum = Integer.MAX_VALUE;
 		for(int i=0; i<arr.length; i++) {
-			for(int j=0; j<arr.length; j++) {
+			for(int j=i+1; j<arr.length; j++) {
 				int sum = arr[i] + arr[j];
 				if(sum < minSum) {
 					minSum = sum;
